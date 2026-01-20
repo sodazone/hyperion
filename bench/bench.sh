66ae1c7d4cc8c1075d4ee6c24567dee116c78277
Marc Fornos <marc.fornos@gmail.com>
2026-01-20 17:12:57 +0100

    update bench script

--- bench/bench.sh
+++ bench/bench.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-wrk -t8 -c50 -d10s 'http://localhost:8080/public/category/TA3941uFAvmVibSkQ6fMJXxmaSNovX86mz/4/0/1792'
+wrk -t8 -c50 -d10s 'http://localhost:8080/v1/public/category/TA3941uFAvmVibSkQ6fMJXxmaSNovX86mz/4/0/1792?exists=true'
+#wrk -t8 -c50 -d10s 'http://localhost:8080/v1/public/category/TA3941uFAvmVibSkQ6fMJXxmaSNovX86mz/4/0/1792'
+#wrk -t8 -c50 -d10s 'http://localhost:8080/v1/public/address/16UZJxDhtiCDhDG9o8txNQRAJLMPDNSJPkactNf9X99HXfuP'
