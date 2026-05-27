2acfbf71ec956537503a094297e4ca2b0af86b74
Marc Fornos <marc.fornos@gmail.com>
2026-05-27 14:06:47 +0200

    Move client source files to browser directory

--- vite.config.js
+++ vite.config.js
@@ -2,28 +2,28 @@ import path from "node:path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	root: path.resolve(__dirname, "src/client"),
-	build: {
-		outDir: path.resolve(__dirname, "public/js"),
-		emptyOutDir: true,
-		sourcemap: true,
-		minify: "esbuild",
-		target: "es2017",
-		rollupOptions: {
-			input: path.resolve(__dirname, "src/client/main.js"),
-			output: {
-				entryFileNames: "main.js",
-				chunkFileNames: "chunk-[hash].js",
-				assetFileNames: "assets/[name]-[hash][extname]",
-				manualChunks(id) {
-					if (id.includes("node_modules")) {
-						return "vendor";
-					}
-				},
-			},
-		},
-	},
-	optimizeDeps: {
-		include: ["alpinejs", "htmx.org"],
-	},
+  root: path.resolve(__dirname, "src/browser"),
+  build: {
+    outDir: path.resolve(__dirname, "public/js"),
+    emptyOutDir: true,
+    sourcemap: true,
+    minify: "esbuild",
+    target: "es2017",
+    rollupOptions: {
+      input: path.resolve(__dirname, "src/browser/main.js"),
+      output: {
+        entryFileNames: "main.js",
+        chunkFileNames: "chunk-[hash].js",
+        assetFileNames: "assets/[name]-[hash][extname]",
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
+  optimizeDeps: {
+    include: ["alpinejs", "htmx.org"],
+  },
 });
