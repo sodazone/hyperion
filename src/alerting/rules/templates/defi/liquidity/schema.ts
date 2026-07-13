271d98805e35d056065a819cec2843159c29bfb7
Xueying Wang <wang.xy.326@gmail.com>
2026-07-13 12:58:41 +0200

    Add Polkadot Asset Hub to supported networks in defi liquidity rule schema

--- src/alerting/rules/templates/defi/liquidity/schema.ts
+++ src/alerting/rules/templates/defi/liquidity/schema.ts
@@ -15,6 +15,10 @@ const supportedNetworks = {
 		{
 			label: "Moonbeam",
 			value: "urn:ocn:ethereum:1284",
+    },
+    {
+			label: "Polkadot Asset Hub",
+			value: "urn:ocn:polkadot:1000",
 		},
 	],
 	multiple: true,
