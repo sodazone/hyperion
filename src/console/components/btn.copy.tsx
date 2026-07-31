d79ffe4680eab289af65090feef0aedb81503a71
Marc Fornos <marc.fornos@gmail.com>
2026-07-31 21:24:00 +0200

    Add width and height to CopyButton SVG

--- src/console/components/btn.copy.tsx
+++ src/console/components/btn.copy.tsx
@@ -17,7 +17,7 @@ export function CopyButton({
           const btn = this;
           navigator.clipboard.writeText('${text}');
           const old = btn.innerHTML;
-          btn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M5 13l4 4L19 7" /></svg>';
+          btn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="${size}" height="${size}" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M5 13l4 4L19 7" /></svg>';
           setTimeout(() => {
             btn.innerHTML = old;
           }, 1200);
